1fbbdd52dfafa74f2d1cf6757b9da6b9e8b46810
matheusned15 <matheusned15@gmail.com>
2021-04-05 14:57:55 -0300

    Adicionando data no atendimento

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,11 +1,15 @@
+const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
     adiciona(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data).format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = { ...atendimento, dataCriacaon, data }
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
-            if(erro) {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+            if (erro) {
                 console.log(erro)
             } else {
                 console.log(resultados)
@@ -13,5 +17,3 @@ class Atendimento {
         })
     }
 }
-
-module.exports = new Atendimento

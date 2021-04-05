1e2fae4485a24dd6f76155e41c1abe6776a0f087
matheusned15 <matheusned15@gmail.com>
2021-04-05 20:23:14 -0300

    Delete

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -67,6 +67,10 @@ class Atendimento {
     }
 
     altera(id, valores, res) {
+
+        if (valores.data) {
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        }
         const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
 
         conexao.query(sql, [valores, id], (erro, resultados) => {
@@ -78,4 +82,17 @@ class Atendimento {
         })
 
     }
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({ id })
+            }
+        })
+    }
+
 }
